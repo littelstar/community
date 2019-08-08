6a93b1bca902f32ae51bf5b5ba2abae0a63a18ad
admin <17864308317@163.com>
2019-08-08 10:33:43 +0800

    usermapper change

--- src/main/java/com/lpp/life/community/mapper/UserMapper.java
+++ src/main/java/com/lpp/life/community/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-
     @Insert("insert into user(login,account_id,token,gmt_create,gmt_modefied) values(#{login},#{accountId},#{token},#{gmtCreate},#{gmtModefied})")
     public void insertUser(User user);
 }
